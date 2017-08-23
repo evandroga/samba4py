e24de70c81645788f2e4953f39753cb1b13f982d
Evandro Gomes <evandroga@hotmail.com>
2017-08-23 16:42:28 -0300

    Update samba4py.py
    Limpesa do código

--- samba4py.py
+++ samba4py.py
@@ -5,13 +5,24 @@ import subprocess
 import sys
 import logging
 
+#Prepara as configurações necessárias para quardar informações
+#em um arquivo de logg chamado 'samba4py.log', presente no
+#diretório raiz do script.
 formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename='samba4py.log', 
                    level=logging.DEBUG,
                    format=formatter)
+#Fim
 
 
 def execProcess(command):
+    '''Executa um processo externo
+    
+    Função que chama um processo externo, mostra a saída
+    no terminal em tempo de execução e guarda a mesma 
+    em arquivo de logg presente no diritório do script.
+    
+    '''
     process = subprocess.Popen(command, shell=True, 
                                stdout=subprocess.PIPE, 
                                stderr=subprocess.STDOUT)
@@ -74,7 +85,6 @@ execProcess("ntpdate a.ntp.br")
 #Adiciona funcionalidades necessárias no arquivo "/etc/fstab" para receber o
 #provisionamento, e remonta a partição raiz "/"
 logging.info('02.1 - PREPARANDO E REMONTANDO A PARTIÇÃO RAIZ ...')
-
 f = open('/etc/fstab', 'r')
 tempstr = f.read()
 f.close()
@@ -83,11 +93,8 @@ tempstr = tempstr.replace("errors=remount-ro","errors=remount-ro,acl,"
 fout = open('/etc/fstab', 'w')
 fout.write(tempstr)
 fout.close()
-
 execProcess("mount -o remount /")
-
 execProcess("clear")
-
 logging.info('02.2 - PARTIÇÃO RAIZ PREPARADA E REMONTADA.')
 #Fim
 
@@ -97,6 +104,7 @@ print "*******************************************************************"
 print "********* PREENCHA AS INFORMAÇÕES A SEGUIR PARA CONTINUAR *********"
 print "*******************************************************************\n"
 
+#Coleta as informações necessárias para iniciar o provisionamento do samba4
 nic = raw_input("Digite o nome da placa de rede a ser usada (ex: eth0): ")
 dns = raw_input("Digite um DNS que será usado para consultas externas "
                 "(ex: 8.8.8.8): ")
@@ -106,6 +114,7 @@ domain = raw_input("Digite o nome curto do domínio (ex: exemplo): ")
 password = raw_input('Crie a senha do usuário "Administrator" do Domínio, \n'
                      'contendo letras, números e caracteres especias '
                      '(ex: Passw0rd): ')
+#Fim
 
 execProcess("clear")
 
@@ -115,30 +124,29 @@ print "*******************************************************************"
 print "********* PROVISIONANDO CONTROLADOR DE DOMÍNIO PRINCIPAL **********"
 print "*******************************************************************\n"
 
+#Realiza os comandos de provisionamento do samba4 com opções extras
+#de limitar a saída do servidor apenas pela placa de rede especificada
 execProcess("samba-tool domain provision --server-role=dc "
             "--dns-backend=SAMBA_INTERNAL --realm="+realm+" "
             "--domain="+domain+" --adminpass="+password+" "
             "--option=\"interfaces=lo "+nic+"\" "
             "--option=\"bind interfaces only=yes\" --function-level=2008_R2")
+#Fim
 
-# Prepara o smb.conf para usar todos os recursos necessários para o servidor
-# funcionar corretamente, já que o mesmo possui opções a mais que as default
+#Prepara o smb.conf para usar todos os recursos necessários para o servidor
+#funcionar corretamente, já que o mesmo possui opções a mais que as default
 logging.info('03.1 - PREPARANDO O ARQUIVO SMB.CONF ...')
-
 f = open('/etc/samba/smb.conf', "r")
 contents = f.readlines()
 f.close()
-
 contents[8] = '        dns forwarder = '+dns+'\n'
 contents[9] = '        server services = s3fs rpc nbt wrepl ldap cldap kdc ' \
               'drepl winbind ntp_signd kcc dnsupdate dns\n\n'
-
 f = open('/etc/samba/smb.conf', "w")
 f.writelines(contents)
 f.close()
-
 logging.info('03.2 - Arquivo smb.conf preparado com sucesso.')
-# Fim
+#Fim
 
 logging.info('03.3 - Instalando arquivo krb5.conf no sistema ...')
 execProcess("cp /var/lib/samba/private/krb5.conf /etc/")
