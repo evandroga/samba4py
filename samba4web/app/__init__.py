b982fd3a7f7c2ea861df9533219253be02acd9d3
Evandro Gomes <evandroga@hotmail.com>
2017-09-01 14:23:39 -0300

    Update __init__.py

--- samba4web/app/__init__.py
+++ samba4web/app/__init__.py
@@ -1,3 +1,4 @@
+import subprocess
 from flask import Flask
 from .auth.login import login
 from .views.users import users
@@ -9,3 +10,13 @@ app.register_blueprint(login)
 app.register_blueprint(users)
 app.register_blueprint(groups)
 app.register_blueprint(gpo)
+
+
+def execProcess(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    out = process.communicate()[0]
+    exitCode = process.returncode
+    output = []
+    for line in out.split('\n'):
+        output.append(line)
+    return output, exitCode
